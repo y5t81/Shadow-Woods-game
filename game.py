92435c0f0d6064661f673224d007c820c5e8f67b
Precious Aderibigbe <y5t81@mercia.keele.ac.uk>
2025-10-30 10:42:00 +0000

    added first choice: cabin or shadow fate

--- game.py
+++ game.py
@@ -1,3 +1,10 @@
 print ("Welome to shadow woods!")
 
-chioce = input("Do you want to go left or right?")
+choice = input("Do you want to go left or right?")
+
+if choice.lower() == "left":
+    print("You discover an old cabin with a glowing door!")
+
+else:
+    print("A tall figure emerges from the trees...")
+    print("You are never seen again. Game over.")
