be6873f44cd312a114609a513d300cb4cb5e503b
Precious Aderibigbe <y5t81@mercia.keele.ac.uk>
2025-10-30 10:51:02 +0000

    add a second feature: enter cabin or vanish

--- game.py
+++ game.py
@@ -5,6 +5,16 @@ choice = input("Do you want to go left or right?")
 if choice.lower() == "left":
     print("You discover an old cabin with a glowing door!")
 
+    second = input("Do you ENTER or IGNORE it? ")
+
+    if second.lower() == "enter":
+        print("Inside, you see a dusty book on a table.")
+
+    else:
+        print("You walk away, but something follows you...")
+        print("You vanish into the darkness. Game over.")
+
+
 else:
     print("A tall figure emerges from the trees...")
     print("You are never seen again. Game over.")
